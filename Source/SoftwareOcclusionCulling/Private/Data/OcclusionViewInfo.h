9ac146e20c17b97f5b649397d09d8c1e11af0f8c
ArmainAP <adrian.popoviciu@katcodelabs.com>
2023-10-08 11:08:55 +0300

    +Added OcclusionViewInfo.h

---
+++ Source/SoftwareOcclusionCulling/Private/Data/OcclusionViewInfo.h
@@ -0,0 +1,40 @@
+﻿// Copyright to Kat Code Labs, SRL. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "IXRTrackingSystem.h"
+#include "Kismet/GameplayStatics.h"
+
+struct FOcclusionViewInfo
+{
+	FOcclusionViewInfo() = default;
+	FOcclusionViewInfo(const APlayerCameraManager* PlayerCameraManager)
+	{
+		const FMinimalViewInfo MinimalView = PlayerCameraManager->GetCameraCacheView();
+		FMatrix ViewProjectionMatrix;
+		UGameplayStatics::GetViewProjectionMatrix(MinimalView, ViewMatrix, ProjectionMatrix, ViewProjectionMatrix);
+		Origin = MinimalView.Location;
+		if (ShouldUseStereoRendering())
+		{
+			ProjectionMatrix = GEngine->StereoRenderingDevice->GetStereoProjectionMatrix(0);	
+		}
+	}
+
+	static bool ShouldUseStereoRendering()
+	{
+#if WITH_EDITOR
+		if (GEditor && !GEditor->IsVRPreviewActive())
+		{
+			return false;
+		}
+#endif
+		
+		return GEngine->StereoRenderingDevice.IsValid() && GEngine->StereoRenderingDevice->IsStereoEnabled() &&
+			GEngine->XRSystem.IsValid() && GEngine->XRSystem->GetHMDDevice();
+	}
+
+	FVector Origin;
+	FMatrix ViewMatrix;
+	FMatrix ProjectionMatrix;
+};
